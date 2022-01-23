13c2ad3c03ce77140fa87b16b554c00a3fb28f02
Sergey Zakrevsky <zak_serge@yahoo.com>
2022-01-23 21:59:36 +0300

    added calc game

---
+++ src/games/calc.js
@@ -0,0 +1,36 @@
+const maxIntervalBorder = 20;
+const gameRules = 'What is the result of the expression?';
+const randomInt = (ceil) => Math.floor(Math.random() * ceil);
+const operationArray = '+-*';
+const getQuestionStr = (arg1, operation, arg2) => `${arg1} ${operationArray[operation]} ${arg2}`;
+const getExpectedAnswer = (arg1, operation, arg2) => {
+  let result;
+  if (operation === 0) {
+    result = arg1 + arg2;
+  }
+  if (operation === 1) {
+    result = arg1 - arg2;
+  }
+  if (operation === 2) {
+    result = arg1 * arg2;
+  }
+  return result;
+};
+const questionBuilder = () => {
+  const leftArg = randomInt(maxIntervalBorder);
+  const rightArg = randomInt(maxIntervalBorder);
+  const sign = randomInt(3);
+  const question = getQuestionStr(leftArg, sign, rightArg);
+  const expectedAnswer = (getExpectedAnswer(leftArg, sign, rightArg).toString());
+  return [question, expectedAnswer];
+};
+
+export default (questionCount) => {
+  const gameData = [];
+  gameData.push(gameRules);
+  for (let counter = 0; counter < questionCount; counter += 1) {
+    gameData.push(questionBuilder());
+  }
+
+  return gameData;
+};
