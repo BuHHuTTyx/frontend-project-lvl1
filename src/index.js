5a45a10566949c59f4f1a2ff024f88cb653a0914
Sergey Zakrevsky <zak_serge@yahoo.com>
2022-01-25 20:57:12 +0300

    cleanup index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import readlineSync from 'readline-sync';
 
-const gameDescription = 'What is the result of the expression?';
-const gameRounds = [['2+2', '4'], ['3+3', '6']];
-const roundsCount = 2;
+export const roundsCount = 3;
 
-const gameProcess = (description, rounds) => {
+export const gameEngine = (description, rounds) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${name}!`);
@@ -17,61 +15,8 @@ const gameProcess = (description, rounds) => {
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
-      return false;
+      return;
     }
   }
   console.log(`Congratulations, ${name}!`);
-  return true;
 };
-gameProcess(gameDescription, gameRounds);
-/*
-const startGame = (rules) => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name?: ');
-  console.log(`Hello, ${name}!`);
-  console.log(rules);
-  return name;
-};
-
-const quiz = (data, maxRounds) => {
-  let rightAnswerCount = 0;
-  do {
-    const [question, expectedAnswer] = data[rightAnswerCount];
-    const answer = readlineSync.question(
-      `Question: ${question}\nYour answer: `,
-    );
-    const isCorrectAnswer = answer === expectedAnswer;
-
-    if (isCorrectAnswer) {
-      console.log('Correct!');
-      rightAnswerCount += 1;
-    } else {
-      console.log(
-        `'${answer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.`,
-      );
-      rightAnswerCount = -1;
-    }
-  } while (rightAnswerCount > 0 && rightAnswerCount < maxRounds);
-  return rightAnswerCount;
-};
-
-const endGame = (name, gameResult, maxRounds) => {
-  if (gameResult === maxRounds) {
-    console.log(`Congratulations, ${name}!`);
-  } else {
-    console.log(`Let's try again, ${name}!`);
-  }
-};
-
-export default (game, roundsCount) => {
-  // const [rules, ...questions] = game;
-  const rules = game[0];
-  const questions = [];
-  for (let counter = 1; counter < game.length; counter += 1) {
-    questions.push(game[counter]);
-  }
-  const name = startGame(rules);
-  const quizResult = quiz(questions, roundsCount);
-  endGame(name, quizResult, roundsCount);
-};
-*/
