af1dd10fc0758768f2b4b2b65a7956d20af7448d
Sergey Zakrevsky <zak_serge@yahoo.com>
2022-01-26 21:58:24 +0300

    refactored calc.js and brain-calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,39 +1,40 @@
-import * as index from '../index.js';
+import { randomInt } from '../utils.js';
+import { roundsCount, engine } from '../index.js';
 
 const maxIntervalBorder = 20;
 const gameRules = 'What is the result of the expression?';
-const randomInt = (ceil) => Math.floor(Math.random() * ceil);
-const operations = '+-*';
-
-const getQuestionStr = (arg1, operation, arg2) => `${arg1} ${operations[operation]} ${arg2}`;
+const operations = ['+', '-', '*'];
 
 const getExpectedAnswer = (arg1, operation, arg2) => {
-  let result;
-  if (operation === 0) {
-    result = arg1 + arg2;
-  }
-  if (operation === 1) {
-    result = arg1 - arg2;
-  }
-  if (operation === 2) {
-    result = arg1 * arg2;
+  let result = 0; // переменная введена для удовлетворения правила линтера consistent-return
+  switch (operation) {
+    case 0:
+      result = arg1 + arg2;
+      break;
+    case 1:
+      result = arg1 - arg2;
+      break;
+    case 2:
+      result = arg1 * arg2;
+      break;
+    // no default
   }
   return result;
 };
 
 const roundBuilder = () => {
-  const leftArg = randomInt(maxIntervalBorder);
-  const rightArg = randomInt(maxIntervalBorder);
-  const sign = randomInt(operations.length);
-  const question = getQuestionStr(leftArg, sign, rightArg);
+  const leftArg = randomInt(0, maxIntervalBorder);
+  const rightArg = randomInt(0, maxIntervalBorder);
+  const sign = randomInt(0, operations.length);
+  const question = `${leftArg} ${operations[sign]} ${rightArg}`;
   const answer = getExpectedAnswer(leftArg, sign, rightArg).toString();
   return [question, answer];
 };
 
 export default () => {
   const gameData = [];
-  for (let counter = 0; counter < index.roundsCount; counter += 1) {
+  for (let counter = 0; counter < roundsCount; counter += 1) {
     gameData.push(roundBuilder());
   }
-  index.gameEngine(gameRules, gameData);
+  engine(gameRules, gameData);
 };
