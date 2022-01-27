d1bbd7970395a55af49d811b59a0550f8be44750
Sergey Zakrevsky <zak_serge@yahoo.com>
2022-01-27 20:21:20 +0300

    refactored calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,33 +1,27 @@
 import { randomInt } from '../utils.js';
 import { roundsCount, engine } from '../index.js';
 
-const maxIntervalBorder = 20;
 const gameRules = 'What is the result of the expression?';
 const operations = ['+', '-', '*'];
 
-const getExpectedAnswer = (arg1, operation, arg2) => {
-  let result = 0; // переменная введена для удовлетворения правила линтера consistent-return
+const calculate = (arg1, operation, arg2) => {
   switch (operation) {
-    case 0:
-      result = arg1 + arg2;
-      break;
-    case 1:
-      result = arg1 - arg2;
-      break;
-    case 2:
-      result = arg1 * arg2;
-      break;
-    // no default
+    case '+':
+      return arg1 + arg2;
+    case '-':
+      return arg1 - arg2;
+    case '*':
+      return arg1 * arg2;
+    default: throw new Error('Unsupported operation');
   }
-  return result;
 };
 
 const roundBuilder = () => {
-  const leftArg = randomInt(0, maxIntervalBorder);
-  const rightArg = randomInt(0, maxIntervalBorder);
-  const sign = randomInt(0, operations.length);
-  const question = `${leftArg} ${operations[sign]} ${rightArg}`;
-  const answer = getExpectedAnswer(leftArg, sign, rightArg).toString();
+  const leftArg = randomInt(0, 20);
+  const rightArg = randomInt(0, 20);
+  const operationIndex = randomInt(0, operations.length);
+  const question = `${leftArg} ${operations[operationIndex]} ${rightArg}`;
+  const answer = calculate(leftArg, operations[operationIndex], rightArg).toString();
   return [question, answer];
 };
 
